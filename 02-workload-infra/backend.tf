2d3898b24338cd3181bc4bce84a6d1273f0a1bf1
Pankaj K Pathania <pankaj_mbsingh@hotmail.com>
2026-06-20 16:28:29 +0530

    fix: remove named profile from backend and provider for CI compatibility

--- 02-workload-infra/backend.tf
+++ 02-workload-infra/backend.tf
@@ -1,16 +1,14 @@
 terraform {
   backend "s3" {
-    bucket                      = "361611338159-tf-state-lab"
-    key                         = "workload/ap-south-1/vpc/terraform.tfstate"
-    region                      = "ap-south-1"
-    encrypt                     = true
-    kms_key_id                  = "arn:aws:kms:ap-south-1:361611338159:key/d518001f-b35d-4078-b541-91d1a78e8915"
-    dynamodb_table              = "terraform-state-lock"
-    profile                     = "workload"
-    dynamodb_endpoint           = "https://dynamodb.ap-south-1.amazonaws.com"
+    bucket         = "361611338159-tf-state-lab"
+    key            = "workload/ap-south-1/vpc/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    kms_key_id     = "arn:aws:kms:ap-south-1:361611338159:key/d518001f-b35d-4078-b541-91d1a78e8915"
+    dynamodb_table = "terraform-state-lock"
 
     assume_role = {
-      role_arn = "arn:aws:iam::361611338159:role/TerraformStateRole-workload"
+      role_arn = "arn:aws:iam::361611338159:role/TerraformCIRole-github"
     }
   }
 }
